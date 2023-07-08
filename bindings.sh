bbb149f33a4b8bd0bb6dd413303d4fcf29b34361
auto update <auto-update@auto-update.auto-update>
2023-07-08 15:48:24 +0300

    auto update

---
+++ bindings.sh
@@ -0,0 +1,8 @@
+# print path and then press Alt-c to cd to that path
+bind '"\ec":"\C-acd \n"'
+
+# Alt-l to ls (not delete prompt)
+bind -x '"\el":"ls -l"'
+
+# Alt-h to history (not delete prompt)
+bind -x '"\eh":"history"'
